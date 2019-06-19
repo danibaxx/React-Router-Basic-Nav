822adb690ef52f57bc0221d6c1cc76b5fd36820e
Danielle B <danibaxx@hotmail.com>
2019-06-18 21:27:21 -0600

    updated Navigation.js with <link> and added route to page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 import { Route } from 'react-router-dom';
 
-const App = () => (
-  <div className="App">
-    <Navigation />
-    <div className="App-container">
-      <Route path="/" exact component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-    </div>
-  </div>
-);
+class App extends React.Component{
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <div className="App-container">
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </div>
+    );
+  }
+};
 
 export default App;
